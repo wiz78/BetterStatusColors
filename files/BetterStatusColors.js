e423d8311f0a8996137584f9b8c0efe92391058a
Simone Tellini <>
2019-05-19 14:10:03 +0200

    Use the same color background also in the issue detail page

--- files/BetterStatusColors.js
+++ files/BetterStatusColors.js
@@ -5,18 +5,47 @@ jQuery( document ).ready( function()
 		var status = jQuery( tr ).find( 'td.column-status i' );
 
 		if( status.length ) {
-			var classes = status.attr( 'class' ).split( /\s+/ );
+			var colorClass = getStatusClass( status );
 
-			classes = jQuery.grep( classes, function( c ) { return /^status-/.test( c ); } );
-
-			if( classes.length ) {
-				var colorClass = classes[ 0 ].replace( '-fg', '-color' );
-
+			if( colorClass ) {
+				
 				status.parentsUntil( 'tr' ).addClass( colorClass );
 				status.hide();
+			
+				jQuery( 'td.column-status > div' ).css( 'color', '#393939' );
 			}
+		}
+	} );
 
-			jQuery( 'td.column-status > div' ).css( 'color', '#393939' );
+	jQuery( '#view-issue-page td.bug-status' ).each( function( i, td )
+	{
+		var status = jQuery( td ).find( '.fa-status-box' );
+
+		if( status.length ) {
+			var colorClass = getStatusClass( status );
+            
+            if( colorClass ) {
+
+				td = jQuery( td );
+
+				td.addClass( colorClass );
+				status.hide();
+
+				td.css( 'color', '#393939' );
+			}
 		}
 	} );
 } );
+
+function getStatusClass( status )
+{
+	var classes = status.attr( 'class' ).split( /\s+/ );
+	var ret = null;
+
+	classes = jQuery.grep( classes, function( c ) { return /^status-/.test( c ); } );
+
+	if( classes.length )
+		ret = classes[ 0 ].replace( '-fg', '-color' );
+
+	return ret;
+}
